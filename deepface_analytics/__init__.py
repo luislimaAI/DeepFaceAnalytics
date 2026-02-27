9ddbc5420d5c8db4b1a044629708200f5f918ace
luislimaAI <limal.2020@alunos.utfpr.edu.br>
2026-02-27 20:14:54 -0300

    feat: [US-003] - Criar analyzer.py com cache TTL e fallback DEEPFACE_AVAILABLE

--- deepface_analytics/__init__.py
+++ deepface_analytics/__init__.py
@@ -1,5 +1,6 @@
 """DeepFace Analytics package."""
 
+from deepface_analytics.analyzer import FaceAnalyzer
 from deepface_analytics.detector import FaceDetector
 
-__all__ = ["FaceDetector"]
+__all__ = ["FaceAnalyzer", "FaceDetector"]
